92bb148f0b70bd24b028fd28bcd7b8d9a077fbbb
praneeth003 <ch.krishnapraneeth@gmail.com>
2024-01-18 12:11:54 -0500

    implement copy to clipboard functionality at receiver

--- src/app/receiver/page.tsx
+++ src/app/receiver/page.tsx
@@ -52,20 +52,33 @@ const Receiver = () => {
         }
     };
 
+    const copyToClipboard = async () => {
+        try {
+            await navigator.clipboard.writeText(answerSignal);
+        }
+        catch (error) {
+            console.error('Error copying to clipboard:', error);
+        }
+    };
+
+
     return (
         <div>
             <h1>Receiver</h1>
 
+            <label htmlFor="receive Offer">Paste the Offer sent by the Sender</label>
             <input
                 type="text"
+                id = "receive Offer"
                 value={receivedSignal}
                 onChange={(e) => setReceivedSignal(e.target.value)}
-                placeholder="Received signal from sender..."
+                placeholder="Offer signal from sender..."
             />
-            <button onClick={handleReceivedSignal}>Receive Signal</button>
+            <button onClick={handleReceivedSignal}>Submit the Offer Signal from the Sender</button>
 
-            <input type='text' value = {answerSignal} placeholder='Answer signal from receiver' ></input>
-            
+            <label htmlFor="answer-signal">Copy the Answer and send it to the Sender</label>
+            <input type='text' value = {answerSignal} placeholder='Answer signal from receiver' id='answer-signal' />
+            <button onClick={copyToClipboard}>Copy to Clipboard</button>
             
 
             {/* Display the received file */}
